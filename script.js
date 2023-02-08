2902b9b2acb1e679ce94df6eb0ebdeefcb49cf1d
Bryan Thompson <114091741+bthompson1223@users.noreply.github.com>
2023-02-08 12:02:28 -0600

    Got js function to choose random color for pixels on page load. Next element will be adding a button to reset the grid and allow user input for total div pixels.

--- script.js
+++ script.js
@@ -1,5 +1,41 @@
+const container = document.querySelector('.container')
 
+function makeStartSquare(rows, cols) {
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
+    for (let i = 0; i < (cols * rows); i++) {
+        let cell = document.createElement('div');
+        container.appendChild(cell).className = "pixel";
+    }
 
-for (let i = 0; i<16; i++) {
-    const pixels = Document.createElement('div')
 }
+
+makeStartSquare(16, 16)
+
+const px = document.querySelectorAll('.pixel');
+
+function getRandomNumber() {
+  return Math.floor(Math.random()*256);
+}
+
+let r = getRandomNumber();
+let g = getRandomNumber();
+let b = getRandomNumber();
+let color = `rgb(${r}, ${g}, ${b})`;
+
+
+/* px.forEach((px) => {
+    px.addEventListener('mouseover', () => {
+        px.style.background = `${color}`;
+        console.log(1)
+    })
+}) */
+
+document.querySelectorAll('.pixel').forEach(item => {
+    item.addEventListener('mouseover', (e) => {
+        item.style.backgroundColor = `${color}`;
+        console.log(color)
+    })
+})
+
+
