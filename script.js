2374fd267cde7174e20c4b52680ae548783cf57b
Bryan Thompson <114091741+bthompson1223@users.noreply.github.com>
2023-02-08 13:41:38 -0600

    Finished js for reset button to accept up to 100x100 grid. Changed some CSS rules to keep the grid the same size and allow the cells to grow or shrink as needed.

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 const container = document.querySelector('.container')
+const px = document.querySelectorAll('.pixel');
+let gridSize = 16
 
-function makeStartSquare(rows, cols) {
+
+const button = document.createElement('button');
+button.classList.add('reset');
+button.textContent = "Reset";
+button.style.cssText = "position: absolute; top: 0; right: 0;"
+container.appendChild(button)
+
+//Function to get random rgb values
+function getRandomNumber() {
+  return Math.floor(Math.random()*256);
+}
+
+//Variables to set random color on page load
+let r = getRandomNumber();
+let g = getRandomNumber();
+let b = getRandomNumber();
+let color = `rgb(${r}, ${g}, ${b})`;
+
+
+function makeStartSquare(gridSize) {
+    let rows = gridSize;
+    let cols = gridSize;
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
     for (let i = 0; i < (cols * rows); i++) {
@@ -8,34 +31,41 @@ function makeStartSquare(rows, cols) {
         container.appendChild(cell).className = "pixel";
     }
 
+    document.querySelectorAll('.pixel').forEach(item => {
+        item.addEventListener('mouseover', (e) => {
+            item.style.backgroundColor = `${color}`;
+            console.log(color)
+        })
+    })
+    
 }
 
-makeStartSquare(16, 16)
 
-const px = document.querySelectorAll('.pixel');
+//Function to get user input for new grid
+function getInput() {
+    let input = window.prompt("Please enter grid size. Size must be between 1 and 100.");
 
-function getRandomNumber() {
-  return Math.floor(Math.random()*256);
+    while (isNaN(input) || input < 1 || input > 100) {
+        input = window.prompt("Sorry, that isn't a valid choice. Please choose a number between 1 and 100.")
+    }
+
+    gridSize = input;
+    removeTiles();
+    makeStartSquare(gridSize)
 }
 
-let r = getRandomNumber();
-let g = getRandomNumber();
-let b = getRandomNumber();
-let color = `rgb(${r}, ${g}, ${b})`;
+function removeTiles() {
+    let box = document.querySelector('.container');
+    while (box.firstChild) {
+        box.removeChild(box.firstChild);
+    }
+}
+
+document.querySelector('.reset').addEventListener('click', getInput)
+
+makeStartSquare(gridSize)
 
 
-/* px.forEach((px) => {
-    px.addEventListener('mouseover', () => {
-        px.style.background = `${color}`;
-        console.log(1)
-    })
-}) */
 
-document.querySelectorAll('.pixel').forEach(item => {
-    item.addEventListener('mouseover', (e) => {
-        item.style.backgroundColor = `${color}`;
-        console.log(color)
-    })
-})
 
 
